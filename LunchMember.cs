012761ad221b7e7c159c993832499af1d4864d83
crisneames <crisneames@gmail.com>
2023-02-14 18:34:41 -0600

    Add project files.

---
+++ LunchMember.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace LunchBuddies;
+
+public class LunchMember
+{
+    public string firstName { get; set; }   
+    public string lastName { get; set; }    
+
+    public LunchMember(string FirstName, string LastName)
+    {
+        firstName = FirstName;
+        lastName = LastName;
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine($"");
+    }
+
+
+}
---
+++ Restaurant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchBuddies
+{
+    public class Restaurant
+    {
+        public string name = "";
+        
+
+        private Restaurant(List<string> Restaurants) 
+        {
+            Restaurants.Add("Chuys");
+            Restaurants.Add("AppleBee's");
+            Restaurants.Add("Cheddars");
+            Restaurants.Add("Slim Chicken");
+            Restaurants.Add("McDonalds");
+
+            Random randomGenerator = new Random();
+            int num = randomGenerator.Next(Restaurants.Count);
+            name = Restaurants[num];
+            Console.WriteLine(name);
+        }
+
+    }
+}
